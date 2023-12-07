fb04492bc148ce9c7933b44bfb87d710ada46dad
shota876 <sh.guliashvili1@gmail.com>
2023-12-07 06:48:37 -0800

    Finished author routes

--- routes/books.js
+++ routes/books.js
@@ -3,12 +3,6 @@ const router = express.Router()
 const Book = require('../models/book')
 const Author = require('../models/author')
 const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
-// const upload = multer({
-//   dest: uploadPath,
-//   fileFilter: (req, file, callback) => {
-//     callback(null, imageMimeTypes.includes(file.mimetype))
-//   }
-// })
 
 // All Books Route
 router.get('/', async (req, res) => {
@@ -54,18 +48,9 @@ router.post('/', async (req, res) => {
     // res.redirect(`books/${newBook.id}`)
     res.redirect(`books`)
   } catch {
-    // if (book.coverImageName != null) {
-    //   removeBookCover(book.coverImageName)
-    // }
     renderNewPage(res, book, true)
   }
 })
-
-// function removeBookCover(fileName) {
-//   fs.unlink(path.join(uploadPath, fileName), err => {
-//     if (err) console.error(err)
-//   })
-// }
 
 async function renderNewPage(res, book, hasError = false) {
   try {
@@ -81,10 +66,10 @@ async function renderNewPage(res, book, hasError = false) {
   }
 }
 
-function saveCover(book, coverEncoded){
-  if(coverEncoded == null) return
+function saveCover(book, coverEncoded) {
+  if (coverEncoded == null) return
   const cover = JSON.parse(coverEncoded)
-  if(coverEncoded != null && imageMimeTypes.includes(cover.type)) {
+  if (cover != null && imageMimeTypes.includes(cover.type)) {
     book.coverImage = new Buffer.from(cover.data, 'base64')
     book.coverImageType = cover.type
   }
